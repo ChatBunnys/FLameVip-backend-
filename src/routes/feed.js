dcced12809fe0edac24768fdb70289a0f7c89bb4
ChatBunnys <234894190+ChatBunnys@users.noreply.github.com>
2026-03-14 19:39:01 -0500

    Format feed.js for consistency and readability

--- src/routes/feed.js
+++ src/routes/feed.js
@@ -13,11 +13,10 @@ router.get("/", verifyToken, (req, res) => {
 
 // create new post
 router.post("/create", verifyToken, (req, res) => {
-
   const { content } = req.body;
 
-  if(!content){
-    return res.status(400).json({ message:"Post content required" });
+  if (!content) {
+    return res.status(400).json({ message: "Post content required" });
   }
 
   const post = {
@@ -30,7 +29,9 @@ router.post("/create", verifyToken, (req, res) => {
   posts.unshift(post);
 
   res.json(post);
-
 });
 
 export default router;
+
+
+  
